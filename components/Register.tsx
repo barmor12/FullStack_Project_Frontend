f206f20867fec620e43cb525de19f42e030268a6
barmor12 <barmo2@ac.sce.ac.il>
2024-05-22 02:14:44 +0300

    register pic

--- components/Register.tsx
+++ components/Register.tsx
@@ -4,14 +4,15 @@ import {
   StyleSheet,
   Text,
   Image,
-  Button as RNButton,
+  TouchableOpacity,
+  ScrollView,
 } from "react-native";
 import { TextInput, Button } from "react-native-paper";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../types";
 import config from "../config";
 import * as ImagePicker from "expo-image-picker";
-import { useGoogleAuth } from "../authService"; // הוספת התחברות דרך גוגל
+import { useGoogleAuth } from "../authService";
 
 type RegisterScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -26,9 +27,9 @@ const RegisterScreen: React.FC<Props> = ({ navigation }) => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [profilePic, setProfilePic] = useState<string>("");
-  const [name, setName] = useState<string>(""); // שדה שם משתמש חדש
+  const [name, setName] = useState<string>("");
   const [error, setError] = useState<string>("");
-  const { promptAsync } = useGoogleAuth(); // הוספת התחברות דרך גוגל
+  const { promptAsync } = useGoogleAuth();
 
   const isValidEmail = (email: string) => {
     const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -86,77 +87,115 @@ const RegisterScreen: React.FC<Props> = ({ navigation }) => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
-      aspect: [4, 3],
+      aspect: [1, 1],
       quality: 1,
     });
 
     if (!result.canceled && result.assets && result.assets.length > 0) {
+      console.log("Selected image URI:", result.assets[0].uri); // דוגמה להוספת הדפסת כתובת התמונה
       setProfilePic(result.assets[0].uri);
     }
   };
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        label="Email"
-        value={email}
-        onChangeText={setEmail}
-        mode="outlined"
-        style={styles.input}
-        autoCapitalize="none"
-      />
-      <TextInput
-        label="Username"
-        value={name}
-        onChangeText={setName}
-        mode="outlined"
-        style={styles.input}
-      />
-      <TextInput
-        label="Password"
-        value={password}
-        onChangeText={setPassword}
-        mode="outlined"
-        secureTextEntry
-        style={styles.input}
-      />
-      <Button mode="elevated" onPress={pickImage} style={styles.button}>
-        Upload Picture
-      </Button>
-      {profilePic ? (
-        <Image source={{ uri: profilePic }} style={styles.image} />
-      ) : null}
-      {error ? <Text style={styles.errorText}>{error}</Text> : null}
-      <Button mode="contained" onPress={handleSignUp} style={styles.button}>
-        Register
-      </Button>
-      <Button
-        mode="contained"
-        onPress={() => promptAsync()} // כפתור התחברות דרך גוגל
-        style={styles.button}
-      >
-        Sign up with Google
-      </Button>
-    </View>
+    <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+      <View style={styles.container}>
+        <TouchableOpacity
+          onPress={pickImage}
+          style={styles.profileImageContainer}
+        >
+          <View style={styles.profileImageWrapper}>
+            <Image
+              source={
+                profilePic
+                  ? { uri: profilePic }
+                  : require("../assets//Profile pic.webp")
+              }
+              style={styles.profileImage}
+            />
+          </View>
+        </TouchableOpacity>
+        <TextInput
+          label="Email"
+          value={email}
+          onChangeText={setEmail}
+          mode="outlined"
+          style={styles.input}
+          autoCapitalize="none"
+        />
+        <TextInput
+          label="Username"
+          value={name}
+          onChangeText={setName}
+          mode="outlined"
+          style={styles.input}
+        />
+        <TextInput
+          label="Password"
+          value={password}
+          onChangeText={setPassword}
+          mode="outlined"
+          secureTextEntry
+          style={styles.input}
+        />
+        {error ? <Text style={styles.errorText}>{error}</Text> : null}
+        <Button mode="contained" onPress={handleSignUp} style={styles.button}>
+          Register
+        </Button>
+        <Button
+          mode="contained"
+          onPress={() => promptAsync()}
+          style={styles.button}
+        >
+          Sign up with Google
+        </Button>
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+  scrollViewContainer: {
+    flexGrow: 1,
+  },
   container: {
     flex: 1,
     justifyContent: "center",
+    alignItems: "center",
     padding: 20,
   },
   input: {
     marginBottom: 10,
+    width: "100%",
   },
   button: {
     marginTop: 20,
+    width: "100%",
   },
-  image: {
-    width: 200,
-    height: 200,
-    marginTop: 10,
+  profileImageContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 20,
+  },
+  profileImageWrapper: {
+    position: "relative",
+  },
+  profileImage: {
+    width: 250,
+    height: 250,
+    borderRadius: 100,
+  },
+  cameraIconContainer: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    backgroundColor: "#ffffff",
+    borderRadius: 20,
+    padding: 5,
+  },
+  cameraIcon: {
+    width: 30,
+    height: 30,
   },
   errorText: {
     color: "red",
